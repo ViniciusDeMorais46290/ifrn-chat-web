70c05cff554e0aa071d96b00340b75c6f4c11f98
Vinicius de Morais <v.morais@escolar.ifrn.edu.br>
2022-05-24 15:53:49 -0300

    Adicionando pagina de Loguot top das galinacias

---
+++ pages/logout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AppContainer,AppInput,AppButton } from "../src/components/index";
+
+
+const Logout = props => {
+  return (
+	<AppContainer>
+        <h1>Está pagina ainda esta em construção 😓</h1>
+		<AppButton title="Clique aqui pra sair da sua conta" value ="Sair" />
+	</AppContainer>
+  );
+};
+
+export default Logout;
